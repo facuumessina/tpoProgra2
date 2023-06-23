aa70ecf569a9a3ad02e5b858f2acab15a5717ea6
Facundo Messina <facuamessina@gmail.com>
2023-06-23 00:55:04 -0300

    Parte 1 init

---
+++ src/impl/ConjuntoEspecial.java
@@ -0,0 +1,43 @@
+package impl;
+
+import tda.ConjuntoEspecialTDA;
+
+public class ConjuntoEspecial implements ConjuntoEspecialTDA {
+
+	@Override
+	public void inicializarConjunto() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Respuesta agregar(int valor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Respuesta sacar(int valor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Respuesta elegir() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean pertenece(int valor) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean conjuntoVacio() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
