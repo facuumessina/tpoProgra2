ecf729eda4136c7e09cc258d62830396903cc83a
Facundo Messina <facuamessina@gmail.com>
2023-06-26 13:34:10 -0300

    Impl done

--- src/impl/DiccionarioSimple.java
+++ src/impl/DiccionarioSimple.java
@@ -9,6 +9,7 @@ public class DiccionarioSimple implements DiccionarioSimpleTDA {
 	ColaPrioridadTDA origen = new ColaPrioridad();
 	
 	/**
+	 * @estrategia Se inicia el diccionario a través de la inicialización de la cola con prioridad.
 	 * @tarea Inicia el diccionario.
 	 * @parámetros No se reciben parámetros.
 	 * @devuelve nada, método void.
@@ -23,12 +24,13 @@ public class DiccionarioSimple implements DiccionarioSimpleTDA {
 	}
 	
 	/**
-	 * @tarea Se valida si la prioridad existe
+	 * @estrategia Se declara una cola con prioridad auxiliar igual a origen para recorrerla en busca de la clave indicada, luego durante el recorrido se determina si sale porque se encontró en el primer lugar, en el último o si no se encontró. Se retornará un booleano determinando si fue encontrado o no.
+	 * @tarea Se valida si la prioridad existe.
 	 * @parámetros Se recibe la clave a validar.
 	 * @devuelve exists: Booleano que representa la existencia o no existencia de la clave en la cola.
 	 * @precondiciones Diccionario inicializado.
 	 * @postcondiciones El método interno retorna el booleano.
-	 * @costo .
+	 * @costo Lineal
 	 */
 	
 	private boolean prioridad2boolean(int clave){
@@ -42,10 +44,10 @@ public class DiccionarioSimple implements DiccionarioSimpleTDA {
 		
 		boolean exists;
 		
-		while (!aux.ColaVacia() && aux.Prioridad()!=clave){ //Se recorre la cola en busca de coincidencia de la clave, en este caso, es la prioridad
+		while (!aux.ColaVacia() && aux.Prioridad()!=clave){ //Se recorre la cola en busca de coincidencia de la clave, en este caso, es la prioridad //Constante
 			prioridad = aux.Prioridad();
 			aux.Desacolar();
-		}
+		}// --> Lineal
 		
 		if(!aux.ColaVacia()) { // validamos si salió debido a que se terminó de recorrer la cola 
 			exists  = true;
@@ -60,12 +62,13 @@ public class DiccionarioSimple implements DiccionarioSimpleTDA {
 		}
 	
 	/**
+	 * @estrategia Se valida a través del método prioridad2boolean si la clave existe, si la clave no existe, se realiza el agregado acolando la prioridad. En caso de existir, se declara una cola auxiliar igual a origen, la misma se recorre hasta encontrar la prioridad indicada, una vez encontrada se realiza el desacolamiento y el posterior acolado para pisar su valor.
 	 * @tarea Se agrega al diccionario una nueva entrada asociada a su clave.
 	 * @parámetros Se recibe la clave junto con el valor a añadir.
 	 * @devuelve nada, método void.
 	 * @precondiciones Diccionario inicializado.
 	 * @postcondiciones Se realiza el ingreso indicado al diccionario.
-	 * @costo .
+	 * @costo Polinómico
 	 */
 
 	@Override
@@ -81,27 +84,28 @@ public class DiccionarioSimple implements DiccionarioSimpleTDA {
 			
 			aux = origen;
 			
-			while (!aux.ColaVacia()) {
+			while (!aux.ColaVacia()) { // Constante
 				
 				if(aux.Prioridad() == clave) {
 					origen.Desacolar();
-					origen.AcolarPrioridad(x, clave);
-				}
+					origen.AcolarPrioridad(x, clave); //Lineal
+				}// --> Lineal
 				
 				aux.Desacolar();
 				
-			}
+			}// --> Polinómico (Se asume el mayor costo posible ya que es el peor)
 		}
 
 	}
 	
 	/**
+	 * @estrategia Se comprueba que la clave ingresada existe a través del método prioridad2boolean, luego si la misma existe, se genera una cola auxiliar y se iguala a origen, para realizar su recorrido en busca de la prioridad, cuando se encuentra la prioridad, se realiza el desacolado en origen.
 	 * @tarea Elimina la clave ingresada del diccionario, si la clave no existe, no se realiza ninguna acción.
 	 * @parámetros Se recibe la clave a eliminar.
 	 * @devuelve nada, método void.
 	 * @precondiciones Diccionario inicializado.
 	 * @postcondiciones Se realiza la eliminación o no.
-	 * @costo .
+	 * @costo Lineal
 	 */
 
 	@Override
@@ -115,26 +119,27 @@ public class DiccionarioSimple implements DiccionarioSimpleTDA {
 			
 			aux = origen;
 			
-			while (!aux.ColaVacia()) {
+			while (!aux.ColaVacia()) { // Constante
 				
 				if(aux.Prioridad() == clave) {
 					origen.Desacolar();
-				}
+				}//Constante
 				
 				aux.Desacolar();
 				
-			}
-		}
+			} // --> Lineal
+		}// --> Lineal
 
 	}
 	
 	/**
+	 * @estrategia Se declara una cola auxiliar y se iguala a origen, además se declara una variable valor a retornar inicializada en 0, se recorre la cola mientras no esté vacía, una vez encontrada la prioridad, se asigna el valor numérico a la variable valor y se retorna dicha variable. 
 	 * @tarea Recupera el valor numérico de la clave solicitada.
 	 * @parámetros Se recibe la clave.
 	 * @devuelve valor: Valor numérico asociado a la clave.
 	 * @precondiciones pila inicializada y clave existente.
 	 * @postcondiciones Se realiza la búsqueda y se retorna el valor numérico asociado a la clave.
-	 * @costo .
+	 * @costo Lineal
 	 */
 
 	@Override
@@ -151,22 +156,23 @@ public class DiccionarioSimple implements DiccionarioSimpleTDA {
 			
 			if(aux.Prioridad() == clave) {
 				valor = aux.Primero();
-			}
+			}// --> Constante
 			
 			aux.Desacolar();
 			
-		}
+		}// --> Lineal
 		
 		return valor;
 	}
 	
 	/**
+	 * @estrategia Se declara e inicializa el conjunto a retornar, además de inicializar una cola auxiliar e igualarla a origen para recorrer. Se recorrerá todo la cola auxiliar agregando las prioridades al conjunto, finalmente se retornará el conjunto indicado.
 	 * @tarea Retorna el conjunto de claves del diccionario.
 	 * @parámetros No se recibe ningún parámetro.
 	 * @devuelve c: El conjunto de claves del diccionario.
 	 * @precondiciones Diccionario inicializado.
 	 * @postcondiciones Se realiza el apilado y se mantiene la estructura valores proveída al método.
-	 * @costo .
+	 * @costo Polinómico
 	 */
 
 	@Override
@@ -180,11 +186,11 @@ public class DiccionarioSimple implements DiccionarioSimpleTDA {
 		
 		aux = origen;
 		
-		while (!aux.ColaVacia()) {
+		while (!aux.ColaVacia()) { // Constante
 			
-			c.Agregar(aux.Prioridad());
+			c.Agregar(aux.Prioridad()); // Lineal
 			aux.Desacolar();
-		}
+		}// --> Polinómico
 		
 		return c;
 	}
--- src/impl/MultiPila.java
+++ src/impl/MultiPila.java
@@ -13,12 +13,13 @@ public class MultiPila implements MultiPilaTDA {
 	Nodo origen;
 	
 	/**
+	 * @estrategia Se recorre la pila recibida asignando los valores de tope a la pila auxiliar previamente declarada. Luego se recorre la pila auxiliar mientras los nuevos valores se apilan sobre los ya existentes y a su vez, se vuelven a apilar los valores en la pila recibida para respetar la estructura recibida.
 	 * @tarea Inserta la pila recibida en el tope de la multipila.
 	 * @parámetros Se recibe la pila valores.
 	 * @devuelve nada, método void.
 	 * @precondiciones pila inicializada.
 	 * @postcondiciones Se realiza el apilado y se mantiene la estructura valores proveída al método.
-	 * @costo .
+	 * @costo Lineal
 	 */
 	
 	@Override
@@ -26,28 +27,29 @@ public class MultiPila implements MultiPilaTDA {
 		PilaTDA aux1 = new Pila();
 		aux1.inicializarPila();
 		
-		while(!valores.pilaVacia()) {
+		while(!valores.pilaVacia()) {// Constante
 			aux1.apilar(valores.tope());
 			valores.desapilar();
-		}
+		}// --> Lineal
 		
-		while(!aux1.pilaVacia()) {
+		while(!aux1.pilaVacia()) {// Constante
 			Nodo nuevo = new Nodo();
 			nuevo.info = aux1.tope();
 			valores.apilar(aux1.tope());
 			nuevo.sig = origen;
 			origen = nuevo;
 			aux1.desapilar();
-		}
+		}// Lineal
 	}
 	
 	/**
+	 * @estrategia Se declara e inicializa una pila auxiliar, se recorre la pila valores mientras la misma no esté vacía y su valor tope coincida con el valor del nodo origen. Mientras eso ocurra, se apilará el valor indicado en la pila auxiliar y se "eliminarán" los nodos coincidentes. Finalmente, se apilarán los valores de la pila auxiliar nuevamente en valores para obtener nuevamente la estructura recibida como parámetro.
 	 * @tarea Desapila la pila recibida por parámetro de la multipila, solo si el tope de la multipila coincide con la pila recibida.
 	 * @parámetros Se recibe la pila valores.
 	 * @devuelve Nada, método void.
 	 * @precondiciones Pila inicializada.
 	 * @postcondiciones Se realiza el desapilado en caso de corresponder y se mantiene la estructura valores proveída al método.
-	 * @costo ¿
+	 * @costo Lineal
 	 */
 
 	@Override
@@ -55,25 +57,26 @@ public class MultiPila implements MultiPilaTDA {
 		PilaTDA aux1 = new Pila();
 		aux1.inicializarPila();
 		
-		while(!valores.pilaVacia() && valores.tope() == origen.info) {
+		while(!valores.pilaVacia() && valores.tope() == origen.info) {// Constante
 				origen = origen.sig;
 				aux1.apilar(valores.tope());
 				valores.desapilar();
-		}
+		}// --> Lineal
 		
-		while(!aux1.pilaVacia()) {
+		while(!aux1.pilaVacia()) {// Constante
 			valores.apilar(aux1.tope());
 			aux1.desapilar();
-		}
+		}// --> Lineal
 	}
 	
 	/**
+	 * @estrategia Se declara e inicializa la pila topes a retornar en el método. Luego se declara un nodo auxiliar igual al de origen, y se inicia un contador "i", se recorren los nodos para saber la cantidad de elementos de la pila incrementando el contador. Se declara un segundo nodo auxiliar igualado nuevamente al origen. Luego si la cantidad ingresada es mayor a la cantidad de nodos de la pila, se recorrerán los nodos nuevamnete y se apilaran todos los topes en la pila a retornar. En el otro caso, se inicia un ciclo while mientras que el valor ingresado sea mayor a 0, se realizará el apilado de los valores numéricos de los nodos y se decrementará el valor de cantidad hasta que sea finalmente 0. Se retorna la pila topes solicitada por el método.
 	 * @tarea Devuelve una pila con los valores que estén en el tope de la multipila. La cantidad de valores a devolver se define por parámetro y debe preservarse el orden.
 	 * @parámetros cantidad: Cantidad de topes a retornar dentro de la pila
 	 * @devuelve topes: Pila que contiene la cantidad de topes solicitados
 	 * @precondiciones Pila inicializada
-	 * @postcondiciones
-	 * @costo 
+	 * @postcondiciones Se retorna la pila con los topes correspondientes solicitada por el método.
+	 * @costo Lineal
 	 */
 
 	@Override
@@ -84,10 +87,10 @@ public class MultiPila implements MultiPilaTDA {
 		Nodo aux = origen;
 		int i =0;
 		
-		while(aux != null) { // Se recorre la pila actual a través de un nodo auxiliar e iteración para determinar la cantidad de valores
+		while(aux != null) { // Se recorre la pila actual a través de un nodo auxiliar e iteración para determinar la cantidad de valores //Constante
 			aux = aux.sig;
 			i++;
-		}
+		}// --> Lineal
 		
 		Nodo aux2 = origen;
 		
@@ -95,13 +98,13 @@ public class MultiPila implements MultiPilaTDA {
 			while (aux2 != null) {
 				topes.apilar(aux2.info);
 				aux2 = aux2.sig;
-			}
+			}//Lineal
 		}else {
 			while (cantidad > 0) {
 				topes.apilar(aux2.info);
 				cantidad--;
-			}
-		}
+			}//Lineal
+		}//Lineal
 		
 		return topes;
 	}
@@ -111,7 +114,7 @@ public class MultiPila implements MultiPilaTDA {
 	 * @parámetros No recibe parámetros de entrada
 	 * @devuelve Nada, método void
 	 * @precondiciones
-	 * @postcondiciones
+	 * @postcondiciones Multipila inicializada
 	 * @costo Constante
 	 */
 
@@ -125,7 +128,7 @@ public class MultiPila implements MultiPilaTDA {
 	 * @parámetros No recibe parámetros de entrada.
 	 * @devuelve Devuelve un booleano que indica si la pila está vacía.
 	 * @precondiciones Pila inicializada.
-	 * @postcondiciones
+	 * @postcondiciones Se retorna un booleano indicando si la pila se encuentra vacía.
 	 * @costo Constante
 	 */
 
