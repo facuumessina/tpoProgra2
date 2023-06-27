2a201970ecf7d9fc3a578b71e68d2d70f6e2d0a4
Facundo Messina <facuamessina@gmail.com>
2023-06-27 00:22:38 -0300

    ex 8 and 11 done

--- src/user/GradoVertice.java
+++ src/user/GradoVertice.java
@@ -1,5 +1,100 @@
 package user;
 
+import impl.Conjunto;
+import tdas.ConjuntoTDA;
+import tda.GrafoTDA;
+
 public class GradoVertice {
+	
+public static int calcularGradoVertice(GrafoTDA grafo, int vertice) {
+	    
+		int grado = 0;
+	    
+	    if (existeVertice(grafo, vertice)) {
+	        
+	    	ConjuntoTDA adyacentesSalida = adyacentesSalida(grafo, vertice);
+	        ConjuntoTDA adyacentesEntrada = adyacentesEntrada(grafo, vertice);
+	        
+	        grado = tamañoConjunto(adyacentesSalida) - tamañoConjunto(adyacentesEntrada);
+	    }
+	    
+	    return grado;
+	}
+	
+	public static ConjuntoTDA adyacentesSalida(GrafoTDA grafo, int vertice) {
+	    
+		ConjuntoTDA adyacentes = new Conjunto(); // Conjunto que almacenar los vértices adyacentes de salida
+	    
+	    if (existeVertice(grafo, vertice)) {
+	        ConjuntoTDA vertices = grafo.Vertices();
+	        
+	        while (!vertices.ConjuntoVacio()) {
+	            int verticeActual = vertices.Elegir();
+	            
+	            if (grafo.ExisteArista(vertice, verticeActual)) {
+	                adyacentes.Agregar(verticeActual);
+	            }
+	            
+	            vertices.sacar(verticeActual);
+	        }
+	    }
+	    
+	    return adyacentes;
+	}
+
+	public static ConjuntoTDA adyacentesEntrada(GrafoTDA grafo, int vertice) {
+	    
+		ConjuntoTDA adyacentes = new Conjunto(); // Conjunto que almacena los vértices adyacentes de entrada
+	    
+	    if (existeVertice(grafo, vertice)) {
+	        ConjuntoTDA vertices = grafo.Vertices();
+	        
+	        while (!vertices.ConjuntoVacio()) {
+	            int verticeActual = vertices.Elegir();
+	            
+	            if (grafo.ExisteArista(verticeActual, vertice)) {
+	                adyacentes.Agregar(verticeActual);
+	            }
+	            
+	            vertices.Sacar(verticeActual);
+	        }
+	    }
+	    
+	    return adyacentes;
+	}
+	
+	public static boolean existeVertice(GrafoTDA grafo, int vertice) {
+	    
+		ConjuntoTDA vertices = grafo.Vertices();
+	    
+	    while (!vertices.ConjuntoVacio()) {
+	        int verticeActual = vertices.Elegir();
+	        vertices.Sacar(verticeActual);
+	        
+	        if (verticeActual == vertice) {
+	            return true;
+	        }
+	    }
+	    
+	    return false;
+	}
+	
+	public static int tamañoConjunto(ConjuntoTDA c) {
+		
+		int contador = 0;
+		
+		ConjuntoTDA aux = new Conjunto();
+		aux.InicializarConjunto();
+		
+		aux = c;
+		
+		while (!aux.ConjuntoVacio()) {
+	        	aux.Sacar(aux.Elegir());
+	        	contador++;
+	    }
+		
+		return contador;
+		
+	}
 
 }
--- src/user/SumaImparABB.java
+++ src/user/SumaImparABB.java
@@ -1,5 +1,82 @@
 package user;
 
+import impl.Conjunto;
+import tdas.ConjuntoTDA;
+
 public class SumaImparABB {
+	
+	/**
+	 * @estrategia Para la realización de este ejercicio, se optó por la estrategia de recorrer todas las claves del diccionario y obtener los valores correspondientes para cada clave. Luego, se recorre el conjunto de valores obtenido para identificar las repeticiones y acolar en "sinrep" los valores correspondientes sin su repetición.
+	 * @tarea Crear una cola que reciba como parámetro un diccionario múltiple y retorne sus valores sin repeticiones.
+	 * @parámetros dic: Diccionario múltiple recibido para crear la cola.
+	 * @devuelve sinrep: Cola generada a partir del diccionario proveído sin repeticiones de valores.
+	 * @precondiciones: Cola inicializada, Diccionario inicializado y no vacío.
+	 * @postcondiciones Se retorna la cola solicitada.
+	 * @costo Polinómico
+	 */
+	
+	public static int sumaImparABB(ABBTDA a) {
+		
+		ConjuntoTDA nodosIm = new Conjunto();
+		nodosIm.InicializarConjunto();
+		
+		nodosIm = nodosImpares(a);
+		
+		int sum = 0;
+		int g;
+		
+		while(!nodosIm.ConjuntoVacio()) {
+			
+			g = nodosIm.Elegir();
+			sum += g;
+			nodosIm.Sacar(g);
+		}
+		
+		return sum;
+	}
+	
+	/**
+	 * @estrategia Para la realización de este ejercicio, se optó por la estrategia de recorrer todas las claves del diccionario y obtener los valores correspondientes para cada clave. Luego, se recorre el conjunto de valores obtenido para identificar las repeticiones y acolar en "sinrep" los valores correspondientes sin su repetición.
+	 * @tarea Crear una cola que reciba como parámetro un diccionario múltiple y retorne sus valores sin repeticiones.
+	 * @parámetros dic: Diccionario múltiple recibido para crear la cola.
+	 * @devuelve sinrep: Cola generada a partir del diccionario proveído sin repeticiones de valores.
+	 * @precondiciones: Cola inicializada, Diccionario inicializado y no vacío.
+	 * @postcondiciones Se retorna la cola solicitada.
+	 * @costo Polinómico
+	 */
+	
+	public static ConjuntoTDA nodosImpares(ABBTDA a){
+		
+		ConjuntoTDA r = new Conjunto();
+		r.InicializarConjunto();
+		
+		if (!a.ArbolVacio()){
+			if (a.Raiz() % 2 != 0){
+				r.agregar(a.Raiz());
+			}
+			
+			ConjuntoTDA rI = nodosImpares(a.HijoIzq());
+		
+			ConjuntoTDA rD = nodosImpares(a.HijoDer());
+		
+			while (!rI.ConjuntoVacio()){
+				
+				int x = rI.Elegir();
+				r.Agregar(x);
+				rI.Sacar(x);
+			}
+		
+			while (!rD.ConjuntoVacio()){
+				
+				int x = rD.Elegir();
+				r.Agregar(x);
+				rD.Sacar(x);
+		
+			}
+		
+		}
+		
+		return r;
+}
 
 }
